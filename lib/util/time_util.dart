c7502bde9bfed10a2e0d30cc91475c2fc29a75d7
JUNGHOON HAN <omaro7@hanmail.net>
2020-05-15 17:47:03 +0900

    Create time_util.dart
    몇 분 전 등의 날짜 처리

---
+++ lib/util/time_util.dart
@@ -0,0 +1,43 @@
+import 'package:timeago/timeago.dart';
+
+class TimeUtil {
+  static setLocalMessages() {
+    setLocaleMessages('ko', KoMessages());
+    setLocaleMessages('de', DeMessages());
+    setLocaleMessages('fr', FrMessages());
+    setLocaleMessages('ja', JaMessages());
+    setLocaleMessages('id', IdMessages());
+    setLocaleMessages('zh', ZhMessages());
+    setLocaleMessages('it', ItMessages());
+    setLocaleMessages('fa', FaMessages());
+    setLocaleMessages('ru', RuMessages());
+    setLocaleMessages('tr', TrMessages());
+    setLocaleMessages('pl', PlMessages());
+  }
+
+  static String timeAgo({int milliseconds}) {
+    final date = DateTime.fromMillisecondsSinceEpoch(milliseconds);
+    return format(date,
+        locale: 'ko', // for test.
+        allowFromNow: true);
+  }
+}
+
+class KoMessages implements LookupMessages {
+  String prefixAgo() => '';
+  String prefixFromNow() => '';
+  String suffixAgo() => '전';
+  String suffixFromNow() => '후';
+  String lessThanOneMinute(int seconds) => '방금';
+  String aboutAMinute(int minutes) => '방금';
+  String minutes(int minutes) => '$minutes분';
+  String aboutAnHour(int minutes) => '1시간';
+  String hours(int hours) => '$hours시간';
+  String aDay(int hours) => '1일';
+  String days(int days) => '$days일';
+  String aboutAMonth(int days) => '한달';
+  String months(int months) => '$months개월';
+  String aboutAYear(int year) => '1년';
+  String years(int years) => '$years년';
+  String wordSeparator() => ' ';
+}
